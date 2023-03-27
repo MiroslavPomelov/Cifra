1cce9955b5e9672a1bbe8d1e00e234e35e72445d
MiroslavPomelov <merosmir@mail.ru>
2023-03-27 20:27:40 +0300

    Create Program.cs

---
+++ 270323/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                string line = Console.ReadLine();
+                int value = int.Parse(line);
+            }
+            catch (Exception exc)
+            {
+                Console.WriteLine("Ошибка: " +exc.Message);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
