c59136f0619c3885ab0946e02af3ee15ec04f205
MiroslavPomelov <merosmir@mail.ru>
2023-03-28 21:03:40 +0200

    Update Program.cs

--- HW/HW/Program.cs
+++ HW/HW/Program.cs
@@ -4,50 +4,77 @@
     {
         static void Main(string[] args)
         {
-            char[] _separators = new char[] { ' ', ',', '.' };
-            string _UserValue;
-            bool word = true;
+            string _directory = "";
+            string _fileName = "Data.txt";
+            string _fullPath = "";
+            List<string> _data = new List<string>();
 
-            CheckIntegerValue("Введите что либо: ");
-            LetterCount("Длинна слова:");
-            WordCount("Количество слов:");
+            _data = ReadFile();
 
-            //Проверка целого или вещественного числа
-            void CheckIntegerValue(string message)
+            while (true)
             {
-                Console.WriteLine(message);
-                _UserValue = Console.ReadLine().Replace(".", ",");
-                float UserValue;
+                PrintData();
+                string line = Console.ReadLine();
 
-                if (float.TryParse(_UserValue, out UserValue))
+                if (line == "exit")
                 {
-
-                    if (UserValue % 1 == 0 || UserValue == 0)
-                    {
-                        Console.Write("Целое число");
-                    }
-                    else
-                    {
-                        Console.Write("Вещественное число");
-                    }
-                    word = false;
+                    break;
                 }
+
+                if (line == "remove")
+                {
+                    Console.Write("Какую строку удаляем? (номер от нуля): ");
+                    int lineNumber = int.Parse(Console.ReadLine());
+                    _data.RemoveAt(lineNumber);
+                    continue;
+                }
+                if (line == "help" || line == "?")
+                {
+                    Console.WriteLine("Выберите команду:\n\"remove\"-удаление строки\n\"exit\"-выйти из программы");
+                    switch (line)
+                    {
+                        case "remove":
+                            Console.WriteLine("Какую строку удаляем? (номер от нуля): ");
+                            int lineNumber = int.Parse(Console.ReadLine());
+                            _data.RemoveAt(lineNumber);
+                            continue;
+                    }
+                }
+
+                AppendData(line);
+                Console.Clear();
             }
 
-            void LetterCount(string message)
+            WriteDataToFile();
+            Console.ReadLine();
+
+
+
+
+            void AppendData(string data)
             {
-                if (word)
-                {
-                    Console.WriteLine();
-                    Console.WriteLine($"{message} {_UserValue.Length}");
-                }
+                _data.Add(data);
             }
 
-            void WordCount(string message)
+            List<string> ReadFile()
             {
-                Console.WriteLine();
-                string[] words = _UserValue.ToString().Split(_separators, StringSplitOptions.RemoveEmptyEntries);
-                Console.WriteLine($"{message} {words.Length}");
+                _directory = Directory.GetCurrentDirectory(); //Путь до exe-файла
+                _directory = new DirectoryInfo(_directory).Parent.Parent.Parent.FullName; //Перейти по каталогу на 3 уровня вверх и получить полный путь
+                _fullPath = Path.Combine(_directory, _fileName); //Комбинировать путь +папка или +файл
+                return File.ReadAllLines(_fullPath).ToList(); //Чтение файла и запись в string переменную
+            }
+
+            void PrintData()
+            {
+                Console.WriteLine("DATA: ");
+                Console.WriteLine(string.Join(Environment.NewLine, _data));
+                Console.WriteLine("--------------------");
+            }
+
+            void WriteDataToFile()
+            {
+                File.WriteAllLines(_fullPath, _data); //Запись string-данных в файл по пути _fullPath
+                Console.WriteLine("Файл сохранен");
             }
         }
     }
