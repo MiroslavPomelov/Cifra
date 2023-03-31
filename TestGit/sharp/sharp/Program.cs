40737420c813b49a4866949896b5a71f7b4960e3
MiroslavPomelov <merosmir@mail.ru>
2023-03-31 20:13:28 +0200

    Create Program.cs

---
+++ TestGit/sharp/sharp/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Data;
+
+namespace sharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int _seconds = 0;
+
+            while (true)
+            {
+                Console.Clear();
+                _seconds = AddSeconds(_seconds);
+                Console.WriteLine($"Время: {GetHours()}:{GetMinute()}:{GetSeconds()}");
+                Console.WriteLine($"Секунды: {_seconds}");
+                Thread.Sleep(1000);
+            }
+
+            Console.ReadLine();
+
+            //END PRPGRAMM
+
+            int AddSeconds(int seconds)
+            {
+                return seconds + GetSeconds();
+            }
+
+            int GetSeconds()
+            {
+                return DateTime.Now.Second;
+            }
+
+            int GetMinute()
+            {
+                return DateTime.Now.Minute;
+            }
+
+            int GetHours()
+            {
+                return DateTime.Now.Hour;
+            }
+        }
+    }
+}
