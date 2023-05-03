141a897f1c955f6a2c54ecbbd516bcf5dacf4229
MiroslavPomelov <merosmir@mail.ru>
2023-05-03 22:21:35 +0200

    Create Program.cs

---
+++ list/list/Program.cs
@@ -0,0 +1,69 @@
+﻿namespace list
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //DateTime dateTime = new DateTime();
+            //Console.WriteLine(dateTime);
+            //Console.WriteLine(DateTime.MinValue);
+            //Console.WriteLine(DateTime.MaxValue);
+            //Console.WriteLine(DateTime.Now);
+            //DateTime dateTime1 = new DateTime(2015, 12, 21);
+            //Console.WriteLine(dateTime1);
+            //DateTime dateTime2 = new DateTime(2016, 6, 15, 12, 45, 25);
+            //Console.WriteLine(dateTime2);
+            //Console.WriteLine(DateTime.UtcNow);
+            //Console.WriteLine(DateTime.Today);
+            //Console.WriteLine(DateTime.Now.DayOfWeek);
+            //Console.WriteLine(DateTime.Now.DayOfYear);
+            //Console.WriteLine(DateTime.Now.Date);
+            //DateTime nowDay = DateTime.Now;
+            //Console.WriteLine(nowDay.AddDays(3));
+            //Console.WriteLine(nowDay.AddMonths(5));
+            //Console.WriteLine(nowDay.Subtract(dateTime1));
+            //Console.WriteLine(nowDay.AddDays(-7));
+            //Console.WriteLine(nowDay.ToLocalTime());
+            //Console.WriteLine(nowDay.ToUniversalTime());
+            //Console.WriteLine(nowDay.ToLongDateString());
+            //Console.WriteLine(nowDay.ToShortTimeString());
+
+
+            //Console.WriteLine("Форматирование дат");
+
+            ////D - полный формат даты
+            ////d - краткий формат
+            ////F - полный формат даты и времени
+            ////f - полный формат даты и карткий времени
+            ////G - краткий формат даты и полный времени
+            ////g - краткий формат даты и времени
+            ////M - формат дней месяца
+            ////O - Формат обратного преобразования даты и времени
+            ////R - Время по Гринвичу
+            ////s - сортируемый формат даты и времни
+            ////T - полный формат времени
+            ////t - краткий формат времени
+            ////U - Полный универсальный формат даты и времени
+            ////Y - формат года
+
+            //Console.WriteLine(nowDay.ToString("D"));
+            //Console.WriteLine(nowDay.ToString("d"));
+
+            //// 1
+
+            //DateTime d1 = new DateTime(2015, 6, 18);
+            //DateTime d2 = new DateTime(2021, 11, 24);
+            //Console.WriteLine((int)d2.Subtract(d1).TotalDays/365);
+
+            //2
+
+            DateTime d3 = new DateTime(2023, 4, 1);
+            DateTime d4 = new DateTime(2023, 3, 1);
+            int hours = d3.Subtract(d4).Hours;
+            int count = int.Parse(Console.ReadLine());
+            int pay = int.Parse(Console.ReadLine());
+            double sum = ((hours / (24.0 / 6) * count * pay) + ((hours / (24.0 / 10) * count * pay * 0, 7) + ((hours / (24.0 / 6) * count * pay * 0, 4));
+
+        }
+    }
+}
