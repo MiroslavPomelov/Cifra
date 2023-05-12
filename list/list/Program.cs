7c2310b0e3cb87645732c5f44124ff0d50b7ed2b
MiroslavPomelov <merosmir@mail.ru>
2023-05-12 18:47:57 +0200

    2

--- list/list/Program.cs
+++ list/list/Program.cs
@@ -57,13 +57,24 @@
 
             //2
 
-            DateTime d3 = new DateTime(2023, 4, 1);
-            DateTime d4 = new DateTime(2023, 3, 1);
-            int hours = d3.Subtract(d4).Hours;
-            int count = int.Parse(Console.ReadLine());
-            int pay = int.Parse(Console.ReadLine());
-            double sum = ((hours / (24.0 / 6) * count * pay) + ((hours / (24.0 / 10) * count * pay * 0, 7) + ((hours / (24.0 / 6) * count * pay * 0, 4));
+            //DateTime d3 = new DateTime(2023, 4, 1);
+            //DateTime d4 = new DateTime(2023, 3, 1);
+            //int hours = d3.Subtract(d4).Hours;
+            //int count = int.Parse(Console.ReadLine());
+            //int pay = int.Parse(Console.ReadLine());
+            //double sum = ((hours / (24.0 / 6) * count * pay) + ((hours / (24.0 / 10) * count * pay * 0, 7) + ((hours / (24.0 / 6) * count * pay * 0, 4));
+
 
         }
+        struct Seanse
+        {
+            public namespace;
+    public double friquency;
+    public DateOnly data;
+    public TimeOnly start;
+    public TimeOnly end;
+    public int count;
+    public string[] mas;
+}
     }
 }
