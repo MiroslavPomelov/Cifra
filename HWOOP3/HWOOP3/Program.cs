1f1cbe3806b0fbd349369089158ab99fe7ae2b9d
MiroslavPomelov <merosmir@mail.ru>
2023-07-17 23:03:47 +0200

    Train HW OOP

---
+++ HWOOP3/HWOOP3/Program.cs
@@ -0,0 +1,4 @@
+﻿using HWOOP3;
+
+Train train = new Train(7, "first", 25);
+train.Print();
---
+++ HWOOP3/HWOOP3/Train.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HWOOP3
+{
+    internal class Train
+    {
+        private int numbervagon;
+        private string clas;
+        private int seats;
+
+        public Train(int numbervagon, string clas, int seats)
+        {
+            this.numbervagon = numbervagon;
+            this.clas = clas;
+            this.seats = seats;
+        }
+
+        public int PassengerOff()
+        {
+            return 10;
+        }
+
+        public int TicketsOff()
+        {
+            return 16;
+        }
+
+        public int SeatsFree()
+        {
+            this.seats -= TicketsOff();
+            this.seats += PassengerOff();
+            return this.seats;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"Свободных мест: {SeatsFree()}");
+        }
+    }
+}
