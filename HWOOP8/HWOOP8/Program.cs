3514b43837e74f6bf007c9a3b52b5c53d6276b47
MiroslavPomelov <merosmir@mail.ru>
2023-08-31 14:40:34 +0200

    Update Program.cs

--- HWOOP8/HWOOP8/Program.cs
+++ HWOOP8/HWOOP8/Program.cs
@@ -1,9 +1,9 @@
 ﻿using HWOOP8;
 
-Mark mark = new Mark(5);
-Exam exam = new Exam("Math");
+Mark mark = new   Mark(5);
+Exam exam = new    Exam("Math");
 Facultet facultet = new Facultet("Economy", exam);
-Abiturient abiturient = new Abiturient("Ivanov V.V.",facultet);
+Abiturient    abiturient = new Abiturient("Ivanov V.V.",facultet);
 Teacher teacher = new Teacher("Petrov A.A.", mark);
 teacher.Print();
 //facultet.ClaimAbiturient();
