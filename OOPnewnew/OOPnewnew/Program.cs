4e4c8c198303a0faee80a2e5db377727ead591eb
Miroslav Pomelov <merosmir@mail.ru>
2023-09-01 20:13:30 +0200

    01.09.2023

---
+++ OOPnewnew/OOPnewnew/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace OOPnewnew
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Human runner = new Human();
+
+            runner.SetAge(50);
+
+            Console.WriteLine(runner.GetAge());
+        }
+    }
+
+    class Human
+    {
+        private int _age;
+        private int _height;
+        private int _weight;
+
+        public void SetAge(int value)
+        {
+            _age = value;
+        }
+
+        public int GetAge()
+        {
+            return _age;
+        }
+    }
+}
