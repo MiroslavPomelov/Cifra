ae345583b6ba9e8260db48f3661db98fab92be9c
Miroslav Pomelov <merosmir@mail.ru>
2023-09-04 18:47:10 +0200

    32

--- OOPnewnew/OOPnewnew/Program.cs
+++ OOPnewnew/OOPnewnew/Program.cs
@@ -4,73 +4,17 @@
     {
         static void Main(string[] args)
         {
-            //Programmer programmer = new Programmer();
-
-            //programmer.SetAge(34);
-            //Console.WriteLine(programmer.GetAge());
-
-            //programmer.brainOverLoad = 40;
-            //Console.WriteLine(programmer.brainOverLoad);
-
-
-            Tel tel = new Tel();
-            Tel secondTel = new Tel();
-
-            Console.WriteLine("Стоимость 1 разговора {0}\nСтоимость 2 разговора {1}", tel.GetCost(4, 0.256), secondTel.GetCost(7, 1.432));
+            
         }
     }
 
-    //class Human
-    //{
-    //    private int _age;
-    //    private int _height;
-    //    private int _weight;
-    //    private int _cost = 100;
-
-    //    public void SetAge(int value)
-    //    {
-    //        _age = value;
-    //    }
-
-    //    public int GetAge()
-    //    {
-    //        return _age;
-    //    }
-    //}
-
-    //class Programmer : Human
-    //{
-    //    public int brainOverLoad;
-    //}
-
-    class Tel
+    class Person
     {
-        private int _dayOfWeek;
-        private double _speechtime;
-        private decimal _cost = 100;
+        public string Name;
 
-
-        public void SetCost(int value)
+        public void ChangeName(string Name)
         {
-            _cost = value;
-        }
-
-        private void CorrectCost(int dayOfWeek, double speechtime)
-        {
-            _dayOfWeek = dayOfWeek;
-            _speechtime = speechtime;
-
-            if (_dayOfWeek > 5 && _dayOfWeek < 8)
-            {
-                _cost = _cost * (decimal)0.9;
-            }
-        }
-
-        public decimal GetCost(int dayOfweek, double speechTime)
-        {
-            CorrectCost(dayOfweek, speechTime);
-
-            return _cost*(decimal)_speechtime;
+            this.Name = Name;
         }
     }
 
