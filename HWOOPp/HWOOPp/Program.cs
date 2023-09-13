af2c446013382ca50408493a73c7ae7610a3f301
MiroslavPomelov <merosmir@mail.ru>
2023-09-13 14:45:28 +0200

    Update Program.cs

--- HWOOPp/HWOOPp/Program.cs
+++ HWOOPp/HWOOPp/Program.cs
@@ -1,6 +1,6 @@
 ﻿using HWOOPp;
 
-MobilePhone mobilePhone = new MobilePhone(89114752355, "Слава", 400);
+MobilePhone mobilePhone = new MobilePhone(89111111111, "Слава", 400);
 
 mobilePhone.AddSum(100);
 mobilePhone.Payment(2, 5);
