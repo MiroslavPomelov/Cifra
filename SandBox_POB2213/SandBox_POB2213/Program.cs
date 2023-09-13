1cd519db7917f6383df175f3801d175f2e33d6da
Miroslav Pomelov <merosmir@mail.ru>
2023-09-13 21:22:15 +0200

    null types

---
+++ SandBox_POB2213/SandBox_POB2213/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace SandBox_POB2213
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = null!;
+
+            name ??= string.Empty; //Сокращенная запись null
+            //?. - проверка на нул предшествующего вызова (все что после знака не работает)
+            //??= - проверка с переопределением
+
+            Console.WriteLine(name.Length);
+        }
+
+        public static int GetQuantity(string parameter)
+        {
+            string result = parameter ?? string.Empty; //?? - Если нулл то пустая строка иначе result = parameter.
+            return result.Length;
+        }
+    }
+
+    //sealed - запрет на наследование класса.
+}
