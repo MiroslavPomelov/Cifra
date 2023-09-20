b91b04f00d95be76960ab1a18bc5d05f8d63f477
Miroslav Pomelov <merosmir@mail.ru>
2023-09-20 21:21:27 +0200

    20.09Polymorph

---
+++ Polymorph20.09/PolymorphPrakt20.09/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace PolymorphPrakt20._09
+{
+    // Отношения классов
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myCar = new Car();
+            myCar.StartCar();
+        }
+    }
+
+    class Engine
+    {
+        public void Start()
+        {
+            Console.WriteLine("Дивгатель запущен");
+        }
+    }
+
+    class Car
+    {
+        private Engine engine;
+
+        public Car()
+        {
+            engine = new Engine();
+        }
+
+        public void StartCar()
+        {
+            engine.Start();
+            Console.WriteLine("Автомобиль начал движение");
+        }
+    }
+}
--- SandBox_POB2213/SandBox_POB2213/Program.cs
+++ SandBox_POB2213/SandBox_POB2213/Program.cs
@@ -4,19 +4,77 @@
     {
         static void Main(string[] args)
         {
-            string name = null!;
+            //string name = null!;
 
-            name ??= string.Empty; //Сокращенная запись null
-            //?. - проверка на нул предшествующего вызова (все что после знака не работает)
-            //??= - проверка с переопределением
+            //name ??= string.Empty; //Сокращенная запись null
+            ////?. - проверка на нул предшествующего вызова (все что после знака не работает)
+            ////??= - проверка с переопределением
 
-            Console.WriteLine(name.Length);
+            //Console.WriteLine(name.Length);
+
+
+
+            //virtual - override - спецификаторы полиморфизм
+            Car[] cars = new Car[3];
+
+            Nissan almera = new Nissan();
+            Chevrolete aveo = new Chevrolete();
+            Lada granta = new Lada();
+
+            cars[0] = almera;
+            cars[1] = aveo;
+            cars[2] = granta;
+
+            for (int i = 0; i < cars.Length; i++)
+            {
+                cars[i].GetCarCountry();
+            }
         }
 
-        public static int GetQuantity(string parameter)
+        //public static int GetQuantity(string parameter)
+        //{
+        //    string result = parameter ?? string.Empty; //?? - Если нулл то пустая строка иначе result = parameter.
+        //    return result.Length;
+        //}
+    }
+
+    class Car
+    {
+        public string CarType { get; set; }
+
+        public virtual void GetCarCountry()
         {
-            string result = parameter ?? string.Empty; //?? - Если нулл то пустая строка иначе result = parameter.
-            return result.Length;
+            Console.WriteLine("Some country");
+        }
+    }
+
+    class Nissan : Car
+    {
+        public string CarType { get; set; }
+
+        public override void GetCarCountry()
+        {
+            Console.WriteLine("Japanese");
+        }
+    }
+
+    class Chevrolete : Car
+    {
+        public string CarType { get; set; }
+
+        public override void GetCarCountry()
+        {
+            Console.WriteLine("USA");
+        }
+    }
+
+    class Lada : Car
+    {
+        public string CarType { get; set; }
+
+        public override void GetCarCountry()
+        {
+            Console.WriteLine("Chinese");
         }
     }
 
