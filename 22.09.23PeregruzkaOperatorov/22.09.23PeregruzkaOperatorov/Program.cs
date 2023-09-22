4a29051cd81e0f8bb9eca099b5703604ecd8422a
Miroslav Pomelov <merosmir@mail.ru>
2023-09-22 21:22:34 +0200

    Abstrakt22.09

---
+++ 22.09.23PeregruzkaOperatorov/22.09.23PeregruzkaOperatorov/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace _22._09._23PeregruzkaOperatorov
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //public static name operator _знак
+
+            SomeClass someClass = new SomeClass();
+            someClass.firstValue = -40;
+            //someClass.secondValue = 30;
+
+
+            Second second = new Second();
+            second.someValue = 25;
+            Console.WriteLine(someClass > second);
+        }
+    }
+
+    class SomeClass
+    {
+        //public int firstValue;
+        //public int secondValue;
+
+        //public static int operator +(SomeClass firstParam, SomeClass secondParam)
+        //{
+        //    return firstParam.firstValue + secondParam.secondValue;
+        //}
+
+        public int firstValue;
+        public static bool operator >(SomeClass first, Second second)
+        {
+            if (first.firstValue < 0)
+            {
+                Console.WriteLine("Число меньше нуля");
+                return false;
+            }
+            return first.firstValue > second.someValue;
+        }
+        public static bool operator <(SomeClass first, Second second)
+        {
+            return first.firstValue < second.someValue;
+        }
+    }
+
+    class First
+    {
+        public int someValue;
+    }
+    class Second
+    {
+        public int someValue;
+    }
+}
---
+++ 22.09.23PeregruzkaOperatorov/PeregruzkaPralt20.09/Program.cs
@@ -0,0 +1,82 @@
+﻿namespace PeregruzkaPralt20._09
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Matrix mat1 = new Matrix();
+            mat1.matrix = new int[3, 3] {
+                {1,2,3 },
+                {4,5,6 },
+                {7,8,9 }
+            };
+
+            mat1.matrix2 = new int[3, 3] {
+                {1,2,3 },
+                {4,5,6 },
+                {7,8,9 }
+            };
+            int[,] array = mat1 - mat1;
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    Console.Write(array[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+
+    class Matrix
+    {
+        public int[,] matrix = new int[3, 3];
+
+        public int[,] matrix2 = new int[3, 3];
+
+        public static int[,] operator +(Matrix matrix, Matrix matrix2)
+        {
+            int[,] matrixResult = new int[3, 3];
+
+            for (int row = 0; row < matrixResult.GetLength(0); row++)
+            {
+                for (int columns = 0; columns < matrixResult.GetLength(1); columns++)
+                {
+                    matrixResult[row, columns] = matrix.matrix[row, columns] + matrix2.matrix2[row, columns];
+                }
+            }
+            return matrixResult;
+        }
+
+        public static int[,] operator -(Matrix matrix, Matrix matrix2)
+        {
+            int[,] matrixResult = new int[3, 3];
+
+            for (int row = 0; row < matrixResult.GetLength(0); row++)
+            {
+                for (int columns = 0; columns < matrixResult.GetLength(1); columns++)
+                {
+                    matrixResult[row, columns] = matrix.matrix[row, columns] - matrix2.matrix2[row, columns];
+                }
+            }
+            return matrixResult;
+        }
+
+        public static int[,] operator *(Matrix matrix, Matrix matrix2)
+        {
+            int[,] matrixResult = new int[3, 3];
+
+            for (int row = 0; row < matrixResult.GetLength(0); row++)
+            {
+                for (int columns = 0; columns < matrixResult.GetLength(1); columns++)
+                {
+                    matrixResult[row, columns] = matrix.matrix[row, columns] * matrix2.matrix2[row, columns];
+                }
+            }
+            return matrixResult;
+        }
+
+    }
+
+}
