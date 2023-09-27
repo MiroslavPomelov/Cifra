10cb7b52287f3b83f2438eb507e93e8a981c84d4
Miroslav Pomelov <merosmir@mail.ru>
2023-09-27 21:16:49 +0200

    27.09AnonymTypess.Delegate.Share

---
+++ 27.09AnonymTypes/27.09AnonymTypes/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace _27._09AnonymTypes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SomeClass someClass = new SomeClass();
+            someClass.ShowSomeValue();
+            someClass.GetSomeValue();
+        }
+    }
+
+    public static class Shower
+    {
+        public static void Show(this int value)
+        {
+            Console.WriteLine(value);
+        }
+
+        public static int Get(this int value)
+        {
+            return value;
+        }
+    }
+
+    class SomeClass
+    {
+        public int someValue = 15;
+
+        public int GetSomeValue()
+        {
+            return someValue.Get();
+        }
+
+        public void ShowSomeValue()
+        {
+            someValue.Show();
+        }
+    }
+    //public static class Shower
+    //{
+    //    public static void Show(this int someValue)
+    //    {
+    //        Console.WriteLine(someValue);
+    //    }
+    //}
+}
---
+++ 27.09AnonymTypes/Prakt2/ExtensionMethods.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prakt2
+{
+    public static class ExtensionMethods
+    {
+        public static string GetMeanMark(this List<(string, int, double)> students)
+        {
+            object[,] marks = new object[students.Count, 2];
+            double[] array = new double[students.Count];
+
+
+            for (int i = 0; i < students.Count; i++)
+            {
+                marks[i, 0] = students[i].Item1;
+                marks[i, 1] = students[i].Item3;
+            }
+
+
+            for (int i = 0; i < students.Count; i++)
+            {
+                array[i] = (double)marks[i, 1];
+            }
+
+            int indexOfMaxValue = Array.IndexOf(array, array.Max());
+            return marks[indexOfMaxValue, 0].ToString();
+        }
+
+        public static double GetAverageAge(this List<(string, int, double)> students)
+        {
+            double[] ages = new double[students.Count];
+            for (int i = 0; i < students.Count; i++)
+            {
+                ages[i] = students[i].Item2;
+            }
+            return ages.Sum() / students.Count;
+        }
+    }
+}
