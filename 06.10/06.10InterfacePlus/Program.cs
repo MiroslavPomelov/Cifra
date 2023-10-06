f3aef25d2f4d3eebecb1ea7b38f009d22d54d77d
Miroslav Pomelov <merosmir@mail.ru>
2023-10-06 21:26:11 +0200

    InterfaceCloneable

---
+++ 06.10/06.10InterfacePlus/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace _06._10InterfacePlus
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person firstPerson = new Person("Slava", 24);
+
+            object secondPerson = firstPerson.Clone();
+
+            Person newSecondPerson = (Person)secondPerson; // Копия того, что в конструкторе выше
+        }
+    }
+
+    //interface Isinger<T> Расширенный интерфейс
+    //{
+    //    T GetValue();
+
+    //    void SetValue(T value);
+    //}
+
+    //class SomeClass: Isinger<int>
+    //{
+    // public int data;
+    // public int GetValue()
+    // {
+    // return ...
+    //}
+
+    //}
+
+    //Интерфейс ICloneable - клонирование типов
+    //Clone() - метод, копирует новый объект в куче.
+
+    class Person : ICloneable
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public object Clone()
+        {
+            return new Person(Name, Age);
+        }
+    }
+}
