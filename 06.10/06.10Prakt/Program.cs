54ccdb2bb8f6b5d8d40893e1c0b6ba61b230487a
Miroslav Pomelov <merosmir@mail.ru>
2023-10-06 19:52:52 +0200

    upd

---
+++ 06.10/06.10Prakt/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace _06._10Prakt
+{
+    public enum Priority
+    {
+        High,
+        Medium,
+        Low
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
+
+    public interface ITask
+    {
+        string Title { get; set; }
+
+        DateTime DueDate { get; set; }
+
+        Priority Priority { get; set; }
+
+        void Display();
+    }
+
+
+    class Task : ITask
+    {
+        public string Title { get; set; }
+        public DateTime DueDate { get; set; }
+        public Priority Priority { get; set; }
+        public void Display()
+        {
+
+        }
+    }
+}
