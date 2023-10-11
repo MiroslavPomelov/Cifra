a91f9dc20d1ea41836973db3b0969d00b1d615a4
Miroslav Pomelov <merosmir@mail.ru>
2023-10-11 21:21:17 +0200

    11.10.2023<T>where

---
+++ 11.10.2023/PraktDelegates11.10/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace PraktDelegates11._10
+{
+    internal class Program
+    {
+        delegate int MyDelegate<T>(T value, T value2);
+        delegate bool MyDelegate1<T>(int boolValue);
+        delegate bool MyDelegate2<T>(int boolValueFact);
+        static void Main(string[] args)
+        {
+            MyDelegate<int> print = delegate (int value, int value2)
+            {
+                return (value * value2);
+            };
+
+            MyDelegate1<bool> printBool = delegate (int boolValue)
+            {
+                if (boolValue % 2 == 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            };
+
+            MyDelegate2<bool> printBoolFact = delegate (int boolValueFact)
+            {
+               return boolValueFact == 0;
+            };
+        }
+    }
+
+
+}
