75abdb5419ac81a0120f6f239f75d0a09c0f5010
Miroslav Pomelov <merosmir@mail.ru>
2023-10-11 19:59:25 +0200

    11.10.2023<T>where

---
+++ 11.10.2023/11.10.2023/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _11._10._2023
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GeneralClass<string> general = new GeneralClass<string>();
+            general.GeneralFieldOne = "Hello";
+            general.GeneralFieldTwo = "Friend";
+
+            ChildClass child = new ChildClass();
+            child.GeneralFieldTwo = 23;
+            child.GeneralFieldOne = 10;
+        }
+
+        //<> where T : - ограничение Т
+    }
+
+    class GeneralClass<T>
+    {
+        public T GeneralFieldOne { get; set; }
+        public T GeneralFieldTwo { get; set;}
+
+    }
+
+    class ChildClass:GeneralClass<int>
+    {
+
+    }
+
+    //public class MyClass<T> where T:class - принимает только класс
+}
---
+++ 11.10.2023/Prakt2_11.09/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Prakt2_11._09
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+
+
+}
