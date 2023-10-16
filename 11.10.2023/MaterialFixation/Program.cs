704ff9ab01956689b2572a45ff9a2a58394bed99
Miroslav Pomelov <merosmir@mail.ru>
2023-10-16 18:37:46 +0200

    d

--- 11.10.2023/MaterialFixation/Program.cs
+++ 11.10.2023/MaterialFixation/Program.cs
@@ -4,10 +4,12 @@
     {
         static void Main(string[] args)
         {
-            
+            User user = new User("Miroslav", "mail.ru","admin");
+            User user2 = new User("Miroslav1", "mail.ru", "admin222");
+            User user3 = new User("Miroslav2", "mail.com", "admin11");
+
+
         }
 
     }
-
-
 }
--- 11.10.2023/MaterialFixation/User.cs
+++ 11.10.2023/MaterialFixation/User.cs
@@ -6,16 +6,76 @@ using System.Threading.Tasks;
 
 namespace MaterialFixation
 {
-    public class User
-    {
+    public delegate string IsDataExists(string data);
 
-    }
-    interface IUser
+
+
+    public class User : IUser
     {
-        int Id {protected get; set; }
-        string Name { protected get; set; }
-        string Login { protected get; set; }
-        string Password { protected get; set; }
+        private string _userDataPath = Directory.GetCurrentDirectory() + "\\users.txt";
+        public int Id { private get; set; }
+        public string Name { private get; set; }
+        public string Login { private get; set; }
+        public string Password { private get; set; }
+
+
+
+        public User(string name, string login, string password)
+        {
+            string[] usersData = File.ReadAllLines(_userDataPath);
+
+            for (int i = 0; i < usersData.Length; i++)
+            {
+                if (usersData[i].Split(" ")[2] == login)
+                {
+                    Console.WriteLine("Пользователь с таким логином уже существует!");
+                    return;
+                }
+                else
+                {
+                    File.WriteAllText(_userDataPath, usersData);
+                }
+            }
+
+            Id = usersData.Length;
+            Name = new IsDataExists((data) =>
+            {
+                if (data == null || data == "")
+                {
+                    throw new Exception("Нет данных");
+                };
+                return data;
+            })
+                (name);
+
+            Login = new IsDataExists((data) =>
+            {
+                if (data == null || data == "")
+                {
+                    throw new Exception("Нет данных");
+                };
+                return data;
+            })
+                (login);
+
+            Password = new IsDataExists((data) =>
+            {
+                if (data == null || data == "")
+                {
+                    throw new Exception("Нет данных");
+                };
+                return data;
+            })
+                (password);
+
+        }
+    }
+    public interface IUser
+    {
+        public int Id { set; }
+        public string Name { set; }
+        public string Login { set; }
+        public string Password { set; }
     }
 
 }
