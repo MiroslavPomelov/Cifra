2a89884e173f917cd5eb69bf2c29c697107df9f8
Miroslav Pomelov <merosmir@mail.ru>
2023-10-18 19:55:33 +0200

    Events18.10.2023

--- 18.10.2023/18.10.2023/Program.cs
+++ 18.10.2023/18.10.2023/Program.cs
@@ -2,11 +2,27 @@
 {
     internal class Program
     {
+        /// <summary>
+        /// События - реакция на внешние действие (уведомление)
+        /// Делегат - учатсник общего кода
+        /// </summary>
+        /// 
+        public static event MyDelegate MyEvent;
         static void Main(string[] args)
         {
-           
+            MyEvent += Displayer;
+
+            if (true)
+            {
+                MyEvent();
+            }
+        }
+        public static void Displayer()
+        {
+            Console.WriteLine("Отработал");
         }
     }
 
+    public delegate void MyDelegate();
 
 }
