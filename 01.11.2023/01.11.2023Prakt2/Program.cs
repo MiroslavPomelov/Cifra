e8b1ee5ab5df5e4ace500f769bea9402502ea960
Miroslav Pomelov <merosmir@mail.ru>
2023-11-01 21:09:02 +0200

    upd

---
+++ 01.11.2023/01.11.2023Prakt2/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace _01._11._2023Prakt2
+{
+    internal class Program
+    {
+        //public static object queue = new object();
+
+        //public static Mutex mutex = new Mutex(); // По сути тот же монитор
+
+        public static Semaphore semaphore = new Semaphore();
+        public static int data = 0;
+
+        static void Main(string[] args)
+        {
+            Thread one = new Thread(Dataencreaser);
+            Thread two = new Thread(Dataencreaser);
+
+            one.Start();
+            two.Start();
+
+            one.Join();
+            two.Join();
+
+            Console.WriteLine(data);
+        }
+        public static void Dataencreaser()
+        {
+            for (int i = 0; i < 100000; i++)
+            {
+                //lock(queue)
+                //  {
+                //      data++;
+                //  }
+
+                //Monitor.Enter(queue); // Аналог - lock
+
+                //try
+                //{
+                //    data++;
+                //}
+                //finally
+                //{
+                //    Monitor.Exit(queue);
+                //}
+
+                //mutex.WaitOne();
+                //data++;
+                //mutex.ReleaseMutex();
+            }
+        }
+    }
+}
+
