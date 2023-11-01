8962a15b20eaf4c56f0a1146f2e4ab5411c1b4fd
Miroslav Pomelov <merosmir@mail.ru>
2023-11-01 19:18:46 +0200

    Update Program.cs

--- 01.11.2023/01.11.2023/Program.cs
+++ 01.11.2023/01.11.2023/Program.cs
@@ -7,19 +7,48 @@
             Thread firstThread = new Thread(Printer);
             Thread secondThread = new Thread(Printer);
             Thread thirdThread = new Thread(Printer);
+            Thread fourthThread = new Thread(Printer);
 
             firstThread.Start(15);
             secondThread.Start("asda");
-            thirdThread.Start(16.324234);
-
-            Console.WriteLine($"Первичный поток ");
+            thirdThread.Start(new Car("dodge","Mc",260));
+            fourthThread.Start(new int[] { 1, 2, 3 });
 
         }
 
         public static void Printer(object obj)
         {
-            Console.WriteLine($"Вторичный поток {obj}");
 
+            if (obj is int[])
+            {
+                foreach (int item in (int[])obj)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+            else if (obj is Car)
+            {
+                Car car = (Car)obj;
+                Console.WriteLine($"Название: {car.CarName}\nТип: {car.CarType}\nСкорость: {car.Speed}");
+            }
+            else
+            {
+                Console.WriteLine(obj);
+            }
+        }
+    }
+
+    class Car
+    {
+        public string CarName { get; set; }
+        public string CarType { get; set; }
+        public double Speed { get; set; }
+
+        public Car(string carName, string carType, double speed)
+        {
+            CarName = carName;
+            CarType = carType;
+            Speed = speed;
         }
     }
 }
