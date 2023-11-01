c868c223d34df1d3dc31709dc07d1ea049428f89
Miroslav Pomelov <merosmir@mail.ru>
2023-11-01 18:48:48 +0200

    01.11.2023

---
+++ 01.11.2023/01.11.2023/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _01._11._2023
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
