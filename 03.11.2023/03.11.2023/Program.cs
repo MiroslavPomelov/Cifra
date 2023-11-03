f6dc1badf5217f086778c03e4f19e43a37154331
Miroslav Pomelov <merosmir@mail.ru>
2023-11-03 21:21:14 +0200

    upd

--- 03.11.2023/03.11.2023/Program.cs
+++ 03.11.2023/03.11.2023/Program.cs
@@ -7,23 +7,55 @@
             //int number = 150;
             //Task myTask = Task.Run(() => DoWork(number));
 
-            Random random = new Random();
-            Task[] tasks = new Task[5];
+            //Random random = new Random();
+            //Task[] tasks = new Task[5];
 
-            for (int i = 0; i < tasks.Length; i++)
+            //for (int i = 0; i < tasks.Length; i++)
+            //{
+            //    tasks[i] = Task.Run(() => { DoWork(random.Next(10, 150)); });
+            //}
+
+            //Task.WaitAll(tasks);
+
+
+
+
+            Task<int[]> task = GetResult();
+            task.Wait();
+
+            int[] megaResult = task.Result;
+
+            foreach (int i in megaResult)
             {
-                tasks[i] = Task.Run(() => { DoWork(random.Next(10, 150)); });
+                Console.WriteLine(i);
             }
 
-            Task.WaitAll(tasks);
         }
 
 
-        public static void DoWork(int number)
+        //public static void DoWork(int number)
+        //{
+        //    Console.WriteLine($"Мне передали число, это - {number}");
+        //    Task.Delay(2000).Wait();
+        //    Console.WriteLine("Работа вторич потока завершена");
+        //}
+
+        public static Task<int[]> GetResult()
         {
-            Console.WriteLine($"Мне передали число, это - {number}");
-            Task.Delay(2000).Wait();
-            Console.WriteLine("Работа вторич потока завершена");
+            Task<int[]> task = Task.Run(() => { return GetArray(10); });
+            return task;
+        }
+
+        public static int[] GetArray(int length)
+        {
+            Random random = new Random();
+            int[] array= new int[length];
+
+            for (int i = 0; i < length; i++)
+            {
+                array[i] = random.Next(1, 10);
+            }
+            return array;
         }
     }
 }
---
+++ 03.11.2023/03.11.2023Prakt/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _03._11._2023Prakt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.GetSumAsync(22, 15);
+
+            Console.ReadKey();
+        }
+
+        public async void GetSumAsync(int firstNumber, int secondNumber)
+        {
+            int result = 0;
+            Console.WriteLine("Метод поиска стартовал!");
+
+            await Task.Run(() =>
+            {
+                result = GetSum(firstNumber, secondNumber);
+            });
+
+            Console.WriteLine($"Сумма двух чисел: {result}");
+        }
+
+        public int GetSum(int firstNumber, int secondNumber)
+        {
+            Task.Delay(2000).Wait();
+            return firstNumber + secondNumber;
+        }
+    }
+}
