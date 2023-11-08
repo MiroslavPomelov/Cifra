97035b2c98c894e545d0281613db450344a307c7
Miroslav Pomelov <merosmir@mail.ru>
2023-11-08 20:03:48 +0200

    Update Program.cs

--- Repeat08.11.2023/Event/Program.cs
+++ Repeat08.11.2023/Event/Program.cs
@@ -2,10 +2,21 @@
 {
     internal class Program
     {
-        public event MyDelegateForEvent MyEvent;
+        public static event MyDelegateForEvent MyEvent;
+        public delegate void MyDelegateForEvent(string message);
         static void Main(string[] args)
         {
+            int[] numbers = new int[] { 1, 2, 3 };
 
+            MyEvent += ShowIfEvent;
+
+            foreach (int i in numbers)
+            {
+                if (i % 2 == 0)
+                {
+                    MyEvent?.Invoke("Четное");
+                }
+            }
         }
 
         public static void ShowIfEvent(string message)
@@ -13,5 +24,4 @@
             Console.WriteLine(message);
         }
     }
-    public delegate void MyDelegateForEvent(string message);
 }
