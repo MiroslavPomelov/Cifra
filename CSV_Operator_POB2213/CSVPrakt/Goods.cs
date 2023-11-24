dc71639f8e0e03e0980a7afd13a680b8be730fb8
Miroslav Pomelov <merosmir@mail.ru>
2023-11-24 19:51:18 +0200

    CSVFiles24.11.2023

---
+++ CSV_Operator_POB2213/CSVPrakt/Goods.cs
@@ -0,0 +1,18 @@
+﻿using CsvHelper.Configuration.Attributes;
+
+namespace CSVPrakt
+{
+
+    public class Goods : IComparable<Goods>
+    {
+
+        public string? Name { get; set; }
+        public int Quantity { get; set; }
+        public int Price { get; set; }
+        public string? Description { get; set; }
+        public int CompareTo(Goods goods)
+        {
+           return this.Price.CompareTo(goods.Price);
+        }
+    }
+}
--- CSV_Operator_POB2213/CSVPrakt/Program.cs
+++ CSV_Operator_POB2213/CSVPrakt/Program.cs
@@ -1,6 +1,8 @@
 ﻿using CsvHelper;
 using System.Globalization;
 using CsvHelper.Configuration;
+using System;
+using System.Linq;
 
 namespace CSVPrakt
 {
@@ -8,7 +10,24 @@ namespace CSVPrakt
     {
         static void Main(string[] args)
         {
-            
+            Goods goods = new Goods();
+            StreamReader reader = new StreamReader("inventoryControl.csv");
+
+            CsvConfiguration csvConfiguration = new CsvConfiguration(CultureInfo.InvariantCulture);
+
+            CsvReader csvReader = new CsvReader(reader, csvConfiguration);
+            List<Goods> people = csvReader.GetRecords<Goods>().ToList();
+
+            reader.Close();
+
+            foreach (var item in people)
+            {
+                Console.WriteLine($"{item.Name}, {item.Quantity}, {item.Price}, {item.Description}");
+            }
+
+            Console.WriteLine("\nМаксимальное значение :" + people.Max(item => item.Price));
+            Console.WriteLine("Минимальное значение :" + people.Min(item => item.Price));
+            Console.WriteLine("Минимальное значение :" + people.Average(item => item.Price));
         }
     }
 }
