b872554ec281541561bd28235bdfa639fceed550
Miroslav Pomelov <merosmir@mail.ru>
2023-12-20 19:47:09 +0200

    DataBase20.12.2023

--- Theme_32_Lesson_4_DataBase_OperatingMethods/Theme_32_Lesson_4_DataBase_OperatingMethods/Program.cs
+++ Theme_32_Lesson_4_DataBase_OperatingMethods/Theme_32_Lesson_4_DataBase_OperatingMethods/Program.cs
@@ -1,8 +1,10 @@
-﻿namespace Theme_32_Lesson_4_DataBase_OperatingMethods
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Theme_32_Lesson_4_DataBase_OperatingMethods
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static /*async Task*/ async void Main(string[] args)
         {
             List<Person> people = new List<Person>();
             //List<Person> people = new List<Person>()
@@ -14,6 +16,7 @@
             //   new Person(){Name = "Alexander", Age = 33}
             //};
 
+
             using (PersonDBContext dBContext = new PersonDBContext())
             {
                 //CREATE
@@ -22,16 +25,48 @@
                 //    dBContext.People.Add(person);
                 //}
 
+                //2 способ
                 //dBContext.People.AddRange(people/*[1], people[2], people[3]*/); // Тот же foreach, 
                 //dBContext.SaveChanges();
 
 
                 //READ
-                people = dBContext.People.ToList();
-                foreach (var person in people)
-                {
-                    Console.WriteLine($"{person.Id}, {person.Name}, {person.Age}");
-                }
+                //people = dBContext.People.ToList();
+                //foreach (var person in people)
+                //{
+                //    Console.WriteLine($"{person.Id}, {person.Name}, {person.Age}");
+                //}
+
+
+                //UPDATE
+                //Person? person = dBContext.People.FirstOrDefault();
+
+                //Person? persons = await dBContext.People.FirstOrDefaultAsync();
+                //Task.Run(async () => { List<Person> persons = await dBContext.People.ToListAsync(); });
+                //*Person? foundPerson = await dBContext.People.FindAsync(1); ---------- Искать по ID
+
+
+                //int counter = dBContext.People.Count(p => p.Name == "Vasya"); -------- Подсчет сущностей по имени.
+
+                //Person? person = new Person();
+                //person.Id = 2; // Изменить определенный объект по ID
+
+                //person.Name = "Slava";
+                //person.Age = 25;
+
+                //dBContext.People.Update(person);
+                //dBContext.SaveChanges();
+
+
+                //DELETE
+                //Person? person = dBContext.People.FirstOrDefault();
+
+                //if (person != null)
+                //{
+                //    dBContext.People.Remove(person);
+                //    dBContext.People.RemoveRange(person);
+                //}
+                //dBContext.SaveChanges();
             }
         }
     }
