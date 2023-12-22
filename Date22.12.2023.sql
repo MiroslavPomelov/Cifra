7d4c600f77210663b1c24efcea2b1d5b4b499507
Miroslav Pomelov <merosmir@mail.ru>
2023-12-22 21:13:04 +0200

    DateBaseDebian

---
+++ Date22.12.2023.sql
@@ -0,0 +1,9 @@
+
+
+CREATE TABLE users(
+userid INT PRIMARY KEY,
+username VARCHAR(50) UNIQUE,
+firstname VARCHAR(50),
+lastname VARCHAR(50),
+age INT,
+email VARCHRA);
---
+++ Date22.12.20231.sql
@@ -0,0 +1,30 @@
+CREATE TABLE users(
+userid INT PRIMARY KEY,
+username VARCHAR(50) UNIQUE,
+firstname VARCHAR(50),
+lastname VARCHAR(50),
+age INT,
+email VARCHAR(50));
+
+INSERT INTO users(userid,username, firstname, lastname, age, email) VALUES(1,'Meros','Miroslav', 'Pomelov','24', 'mail@mail.ru'),(2,'Mal','Slava', 'Mohin','32', 'mail32@mail.ru'),(3,'Kolyan','Nikolay', 'Ivanov','40', 'mailssss@mail.ru');
+
+SELECT * FROM users;
+
+CREATE TABLE fruits(
+fruitid INT PRIMARY KEY,
+fruitname VARCHAR(50),
+variety VARCHAR(50)  UNIQUE,
+color VARCHAR(50),
+taste VARCHAR(50));
+
+
+INSERT INTO fruits(fruitid,fruitname, variety, color, taste) VALUES(1,'Apple', 'variety','green', 'tasty'),(2,'Orange', 'variety2','yellow', 'sweet'),(3,'Lemon', 'hhh','yellow', 'sour');
+
+SELECT * FROM fruits;
+
+CREATE TABLE userfruits(
+userfruitid INT PRIMARY KEY,
+userid INT REFERENCES users (userid) ,
+fruitid INT REFERENCES fruits (fruitid));
+
+SELECT * FROM userfruits;fruits
