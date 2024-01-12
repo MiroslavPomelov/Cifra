994203a98de99d5c0992ec3239f160ab88a822ea
Miroslav Pomelov <merosmir@mail.ru>
2024-01-12 21:21:09 +0200

    Theme_35_Lesson_1_12.01.2024

---
+++ Theme_35_Lesson_1_Prakt_12.01.2024/2/Movies.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CsvHelper.Configuration.Attributes;
+
+namespace _2
+{
+    public class Movies
+    {
+        [Name("Id")]
+        public int Id { get; set; }
+        [Name("Name")]
+        public string Name { get; set; }
+        [Name("Year")]
+        public int Year { get; set; }
+        [Name("Director")]
+        public string Director { get; set; }
+        [Name("Genre")]
+        public string Genre { get; set; }
+        [Name("Graduate")]
+        public double Graduate { get; set; }
+    }
+}
---
+++ Theme_35_Lesson_1_Prakt_12.01.2024/2/Program.cs
@@ -0,0 +1,35 @@
+﻿using CsvHelper.Configuration;
+using CsvHelper;
+using System.Globalization;
+using System;
+
+namespace _2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //IOrderedEnumerable<> sorted = .OrderBy(IThreadPoolWorkItem => item).ThenBy()
+
+            using StreamReader reader = new StreamReader("Movies.csv");
+
+            CsvConfiguration csvConfiguration = new CsvConfiguration(CultureInfo.InvariantCulture)
+            {
+                Delimiter = "," 
+            };
+            CsvReader csvReader = new CsvReader(reader, csvConfiguration);
+
+            List<Movies> movies = csvReader.GetRecords<Movies>().ToList();  
+
+            reader.Close();
+
+            //IOrderedEnumerable<Movies> sorted = movies.OrderBy(item => item).ThenBy(item => item.Name).
+            IEnumerable<Movies> extensionMethodResult = movies.Where(mov => mov.Name.StartsWith("The")).OrderBy(item => item.Genre);
+
+            foreach (var item in extensionMethodResult)  
+            {
+                Console.WriteLine($"Номер: {item.Id} Название: {item.Name} Режиссер: {item.Director} Жанр: {item.Genre} Оценка {item.Graduate}");
+            }
+        }
+    }
+}
