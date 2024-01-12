1651ead038122cfb8722e3dbd87344cf8883a8d4
Miroslav Pomelov <merosmir@mail.ru>
2024-01-12 20:47:14 +0200

    Theme_35_Lesson_1_12.01.2024

---
+++ Theme_35_Lesson_1_12.01.20224/2/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace _2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Upcast
+            List<Animal> animals = new List<Animal>()
+            {
+                new Animal("Animal",20),
+                new Dog("Chaffie", 10, "Woof"),
+                new Cat("Barsik", 7, "Meow"),
+                new Dragon("Smaug", 1500, "AAA")
+            };
+
+            // Фильрация линк
+            var items = animals.OfType<Dog>();
+
+            foreach (var item in items)
+            {
+                Console.WriteLine(item.Name);
+            }
+        }
+    }
+
+    public class Animal
+    {
+        public string Name { get; set; }
+        public double Weight { get; set; }
+
+        public Animal(string name, double weight)
+        {
+            Name = name;
+            Weight = weight;
+        }
+    }
+
+    public class Dog : Animal
+    {
+        public string Noise { get; set; } = "Woof";
+        public Dog(string name, double weight, string noise) : base(name, weight)
+        {
+            Noise = noise;
+        }
+    }
+
+    public class Cat : Animal
+    {
+        public string Noise { get; set; } = "Meow";
+        public Cat(string name, double weight, string noise) : base(name, weight)
+        {
+            Noise = noise;
+        }
+    }
+
+    public class Dragon : Animal
+    {
+        public string Noise { get; set; } = "AAAAAA";
+        public Dragon(string name, double weight, string noise) : base(name, weight)
+        {
+            Noise = noise;
+        }
+    }
+}
--- Theme_35_Lesson_1_Prakt_12.01.2024/Theme_35_Lesson_1_Prakt_12.01.2024/Program.cs
+++ Theme_35_Lesson_1_Prakt_12.01.2024/Theme_35_Lesson_1_Prakt_12.01.2024/Program.cs
@@ -30,24 +30,32 @@ namespace Theme_35_Lesson_1_Prakt_12._01._2024
             var events = new List<Event>
             {
                 new Event { Id = 1, Name = "Презентация новых продуктов",Date = DateTime.Now.AddDays(10),Priority = Priority.High },
-                new Event { Id = 2, Name = "Обучающий вебинар по программированию", Date = DateTime.Now.AddMonths(1), Priority = Priority.High },
-                new Event { Id = 3, Name = "Выставка искусства", Date = DateTime.Now.AddDays(20), Priority = Priority.High },
+                new Event { Id = 2, Name = "Обучающий вебинар по программированию", Date = DateTime.Now.AddMonths(1), Priority = Priority.Low },
+                new Event { Id = 3, Name = "Выставка искусства", Date = DateTime.Now.AddDays(20), Priority = Priority.Low },
                 new Event { Id = 4, Name = "Спортивный турнир по футболу", Date = DateTime.Now.AddDays(30), Priority = Priority.High },
-                new Event { Id = 5, Name = "Фестиваль культурных традиция", Date = DateTime.Now.AddMonths(2), Priority = Priority.High },
-
-                new Event { Id = 1, Name = "Научная коференция по биологии",Date = DateTime.Now.AddMonths(3),Priority = Priority.High },
-                new Event { Id = 2, Name = "Празднования дня города", Date = DateTime.Now.AddMonths(4), Priority = Priority.High },
-                new Event { Id = 3, Name = "Концерт мировых хитов", Date = DateTime.Now.AddDays(25), Priority = Priority.High },
-                new Event { Id = 4, Name = "Выставка технологических новинок", Date = DateTime.Now.AddMonths(2), Priority = Priority.High },
-                new Event { Id = 5, Name = "Обучающий семинар по управлению временем", Date = DateTime.Now.AddDays(12), Priority = Priority.High }
+                new Event { Id = 5, Name = "Фестиваль культурных традиция", Date = DateTime.Now.AddMonths(2), Priority = Priority.Medium },
+                new Event { Id = 6, Name = "Научная коференция по биологии",Date = DateTime.Now.AddMonths(3),Priority = Priority.High },
+                new Event { Id = 7, Name = "Празднования дня города", Date = DateTime.Now.AddMonths(4), Priority = Priority.Medium },
+                new Event { Id = 8, Name = "Концерт мировых хитов", Date = DateTime.Now.AddDays(25), Priority = Priority.High },
+                new Event { Id = 9, Name = "Выставка технологических новинок", Date = DateTime.Now.AddMonths(2), Priority = Priority.High },
+                new Event { Id = 10, Name = "Обучающий семинар по управлению временем", Date = DateTime.Now.AddDays(12), Priority = Priority.High }
             };
 
-            IEnumerable<string> result = events.Where(good => good.Date < DateTime.Now.AddMonths(1)).Select(good => good.Name)!;
+            IEnumerable<string> concreteEvents = events.Where(good => good.Date < DateTime.Now.AddMonths(1) && good.Priority == Priority.High).Select(good => good.Name)!;
 
-            foreach (var item in result)
+            // Фильтрация линк по нескольким критериям
+            IEnumerable<Event> concreteLinqEvents = from i in events
+                                                 where i.Date >= new DateTime(2024, 02, 01) 
+                                                 where i.Priority == Priority.High
+                                                 select i;
+
+            foreach (var item in concreteLinqEvents)
             {
-                Console.WriteLine(item);
+                Console.WriteLine(item.Name);
             }
+
+            // Фильтрация линк на методах расширения
+            IEnumerable<Event> concreteExtensionEvents = events.Where(e => e.Date >= new DateTime(2024, 02, 01)).Where(e => e.Priority == Priority.High).Select(e => e);
         }
     }
 }
