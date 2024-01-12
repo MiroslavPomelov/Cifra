7da7255a941c17f152b2a806217fed72e374a69c
Miroslav Pomelov <merosmir@mail.ru>
2024-01-12 19:02:29 +0200

    12.01.2024

---
+++ Theme_35_Lesson_1_12.01.20224/Theme_35_Lesson_1_12.01.20224/Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Theme_35_Lesson_1_12._01._20224
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Price{ get; set; }
+        public int Quantity { get; set; }
+
+        public Product(int id, string name, string description, double price, int quantity)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+            Price = price;
+            Quantity = quantity;
+        }
+    }
+}
---
+++ Theme_35_Lesson_1_12.01.20224/Theme_35_Lesson_1_12.01.20224/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Theme_35_Lesson_1_12._01._20224
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> products = new List<Product>()
+            {
+                new Product(0, "Телевизор", "Мощный телек", 1500.0, 25),
+                new Product(1, "Пылесос", "Мощный пылесос", 200.0, 20),
+                new Product(2, "Утюг", "Паровой", 15000.0, 16),
+                new Product(3, "Холодильник", "Большой", 12900.0, 40),
+                new Product(4, "Шуруповерт", "Не для гвоздей", 35000.0, 125),
+            };
+
+            //IEnumerable<Product> items = products.Where(p => p.Price < 1000.0); // Для выборки данных по условию лямбда
+
+            //foreach (var item in items)
+            //{
+            //    Console.WriteLine(item.Name);
+            //}
+
+            IEnumerable<string> result = from good in products
+                                         where good.Price < 1000.0
+                                         select good.Name;
+        }
+    }
+}
