c566e88e401e24039b609949fc60e9e66753f103
Miroslav Pomelov <merosmir@mail.ru>
2024-01-29 20:06:10 +0200

    WPF_29.01.2024

---
+++ WPF_29.01.2024/WPF_Prakt_29.01.2024/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_Prakt_29._01._2024
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            SolidColorBrush colorBrush = new SolidColorBrush(Colors.Blue);
+            Red1.Fill = colorBrush;
+            Red2.Fill = colorBrush;
+            Red3.Fill = colorBrush;
+        }
+    }
+}
