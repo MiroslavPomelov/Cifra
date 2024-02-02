310f3753bcc41365aa8e4cd73d03923c040f187d
Miroslav Pomelov <merosmir@mail.ru>
2024-02-02 21:09:56 +0200

    WPF_02.02.2024_DataBinding

--- WPF_02.02.2024/WPF_Lesson88_DataBinding/MainWindow.xaml.cs
+++ WPF_02.02.2024/WPF_Lesson88_DataBinding/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,16 +24,78 @@ namespace WPF_Lesson88_DataBinding
         public MainWindow()
         {
             InitializeComponent();
+
+            Person man1 = (Person)Resources["first"];
+            man1.Name = "asdasdad";
+            man1.Surname = "Casdasdf";
+
+            Person man = (Person)Resources["second"];
+            man.Name = "Святослав";
+            man.Surname = "Cfcfcf";
+
         }
     }
 
-    public class Person
+    public class Person : INotifyPropertyChanged
     {
-        public string Name { get; set; }
-        public string Surname { get; set; }
-        public int Age { get; set; }
-        public string Birthday { get; set; }
+        private string _name;
+        private string _surname;
+        private string _age;
+        private string _birthday;
+        public string Name 
+        { 
+            get { return _name; }
+            set 
+            {
+                if (_name != value)
+                {
+                    _name = value;
+                    OnPropertyChanged(nameof(Name));
+                }
+            }
+        }
+        public string Surname
+        {
+            get { return _surname; }
+            set
+            {
+                if (_surname != value)
+                {
+                    _surname = value;
+                    OnPropertyChanged(nameof(Surname));
+                }
+            }
+        }
+        public string Age
+        {
+            get { return _age; }
+            set
+            {
+                if (_age != value)
+                {
+                    _age = value;
+                    OnPropertyChanged(nameof(Age));
+                }
+            }
+        }
+        public string Birthday
+        {
+            get { return _birthday; }
+            set
+            {
+                if (_birthday != value)
+                {
+                    _birthday = value;
+                    OnPropertyChanged(nameof(Birthday));
+                }
+            }
+        }
 
-
+        public event PropertyChangedEventHandler? PropertyChanged;
+        
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
