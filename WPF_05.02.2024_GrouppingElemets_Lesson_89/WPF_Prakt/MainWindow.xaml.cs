3fed8ad33c0973ac699eabbc10a4d852c20f680e
Miroslav Pomelov <merosmir@mail.ru>
2024-02-05 20:32:01 +0200

    WPF_05.02.2024_Groupping

---
+++ WPF_05.02.2024_GrouppingElemets_Lesson_89/WPF_Prakt/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.DirectoryServices.ActiveDirectory;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_Prakt
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+
+    public class Data : INotifyPropertyChanged
+    {
+        private ObservableCollection<Person> _myCollection;
+        public ObservableCollection<Person> MyCollection
+        {
+            get { return _myCollection; }
+            set
+            {
+                if (_myCollection != value)
+                {
+                    _myCollection = value;
+                    OnPropertyChanged(nameof(MyCollection));
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+    }
+}
