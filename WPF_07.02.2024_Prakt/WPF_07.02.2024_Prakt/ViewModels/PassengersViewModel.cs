98bf5a66fcd4e13810dcb5767ac8ce36bf16c7f6
Miroslav Pomelov <merosmir@mail.ru>
2024-02-07 21:14:32 +0200

    WPF_07.02.2024_Prakt

---
+++ WPF_07.02.2024_Prakt/WPF_07.02.2024_Prakt/ViewModels/PassengersViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WPF_07._02._2024_Prakt.Model;
+using WPF_07._02._2024_Prakt.ViewModels.Base;
+
+namespace WPF_07._02._2024_Prakt.ViewModels
+{
+    public class PassengersViewModel : ViewModelBase
+    {
+        private ObservableCollection<TitanicPassanger> _passangers;
+
+        public ObservableCollection<TitanicPassanger> Passangers
+        {
+            get { return _passangers; }
+            set
+            {
+                if (_passangers != value)
+                {
+                    _passangers = value;
+                    OnPropertyChanged(nameof(Passangers));
+                }
+            }
+        }
+
+        public PassengersViewModel()
+        {
+            Passangers = new ObservableCollection<TitanicPassanger>();
+        }
+    }
+}
