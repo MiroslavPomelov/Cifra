53c6b80fdbcbb52d623f8dd8c13f54e02b36a7ff
MiroslavPomelov <128490138+MiroslavPomelov@users.noreply.github.com>
2024-03-15 20:13:33 +0200

    StructPattern_15.03.2024

---
+++ StructPattern15.03.2024/Program.cs
@@ -0,0 +1,13 @@
+﻿using StructPattern15._03._2024;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Human baby1 = new Baby();
+
+        baby1 = new ProgressDecorator(baby1);
+
+        baby1.DisplayInfo();
+    }
+}
