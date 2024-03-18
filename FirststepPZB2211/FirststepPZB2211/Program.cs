c3c4056ab34d8751a7dccd0008676bb29c91199a
Miroslav Pomelov <merosmir@mail.ru>
2024-03-18 20:07:54 +0200

    Pattern_18.03.2024povedenie

---
+++ FirststepPZB2211/FirststepPZB2211/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace FirststepPZB2211
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string phrase = Console.ReadLine();
+            for (int counter = 0; counter < phrase.Length; counter++)
+            {
+                Console.WriteLine(phrase[counter]);
+            }
+
+
+
+
+
+
+
+        }
+    }
+}
