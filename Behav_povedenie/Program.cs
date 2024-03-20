7064c8a9033a1ebeb023e30d0936ae0a75c0a25d
MiroslavPomelov <128490138+MiroslavPomelov@users.noreply.github.com>
2024-03-20 21:19:50 +0200

    Behav_Patter_20.03.2024

---
+++ Behav_povedenie/Program.cs
@@ -0,0 +1,70 @@
+﻿using System.Globalization;
+
+namespace Behav_povedenie
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+
+    public interface IMediatorBase
+    {
+        void SendData(Collegue collegue);
+        void GetData(Collegue collegue);
+    }
+
+    public abstract class Collegue
+    {
+        public void GetData(string data)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SendData(string data)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class Manager : IMediatorBase
+    {
+        public void GetData(Collegue collegue)
+        {
+            if (collegue is Officiant)
+            {
+                
+            }
+            else
+            {
+
+            }
+        }
+
+        public void SendData(Collegue collegue)
+        {
+            if (true)
+            {
+
+            }
+        }
+    }
+
+    public class Officiant : Collegue
+    {
+        public string Order { get; set; }
+
+        public Officiant(string order)
+        {
+            Order = order;
+        }
+
+    }
+
+    public class Cooker : Collegue
+    {
+
+    }
+}
