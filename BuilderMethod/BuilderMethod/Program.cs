094d4faad87fd54286d61f251900c70f48f180bb
MiroslavPomelov <merosmir@mail.ru>
2024-03-23 11:18:48 +0200

    Builder_Fabric_pattern

---
+++ BuilderMethod/BuilderMethod/Program.cs
@@ -0,0 +1,153 @@
+﻿using System.ComponentModel;
+
+namespace BuilderMethod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IAssempler director = new CustomSquadAssembler();
+
+            IPersonBuilder personBuilder = new SquadBuilder();
+            director.Assembly(personBuilder);
+
+            CompletePerson completePerson = personBuilder.GetComplect();
+            completePerson.Show();
+
+        }
+    }
+
+    class CompletePerson
+    {
+        public List<Person> Person { get; } = new List<Person>();
+
+        public void AddPerson(Person item)
+        {
+            Person.Add(item);
+        }
+
+        public void Show()
+        {
+            Console.WriteLine("Создание:");
+            foreach (var item in Person)
+            {
+                Console.WriteLine($"- {item.Name} {item.Class}");
+            }
+        }
+    }
+
+    // Строитель
+    interface IPersonBuilder
+    {
+        IPersonBuilder BuildWarrior();
+        IPersonBuilder BuildArcher();
+        IPersonBuilder BuildMage();
+        CompletePerson GetComplect();
+    }
+
+    // Конкретный строитель
+    class SquadBuilder : IPersonBuilder
+    {
+        private CompletePerson _set = new CompletePerson();
+
+        public IPersonBuilder BuildWarrior()
+        {
+            _set.AddPerson(new Warrior("Warrior",10,"Воин"));
+            return this;
+        }
+
+        public IPersonBuilder BuildArcher()
+        {
+            _set.AddPerson(new Archer("Archer", 15, "Лучник"));
+            return this;
+        }
+
+        public IPersonBuilder BuildMage()
+        {
+            _set.AddPerson(new Mage("Mage", 12, "Маг"));
+            return this;
+        }
+
+        public CompletePerson GetComplect()
+        {
+            return _set;
+        }
+    }
+
+    interface IAssempler
+    {
+        void Assembly(IPersonBuilder builder);
+    }
+
+    // Директор
+    class CustomSquadAssembler : IAssempler
+    {
+        public void Assembly(IPersonBuilder builder)
+        {
+            builder.BuildWarrior()
+                   .BuildArcher()
+                   .BuildMage();
+        }
+    }
+
+    // Базовый класс для персонажей
+    abstract class Person
+    {
+        public string Name { get; set; }
+        public string Class { get; set; }
+
+        protected Person(string name, string clas)
+        {
+            Name = name;
+            Class = clas;
+        }
+    }
+
+    // Войн
+    class Warrior : Person
+    {
+        public string Name { get; set; }
+        public int lvl { get; set; }
+        public int clas { get; set; }
+
+
+        public Warrior(string name, int lvl, string clas)
+            : base(name, clas)
+        {
+            Name = name;
+            this.lvl = lvl;
+        }
+    }
+
+    // Лучник
+    class Archer : Person
+    {
+        public string Name { get; set; }
+        public int lvl { get; set; }
+        public int clas { get; set; }
+
+
+        public Archer(string name, int lvl, string clas)
+            : base(name, clas)
+        {
+            Name = name;
+            this.lvl = lvl;
+        }
+    }
+
+    // Маг
+    class Mage : Person
+    {
+        public string Name { get; set; }
+        public int lvl { get; set; }
+        public int clas { get; set; }
+
+
+        public Mage(string name, int lvl, string clas)
+            : base(name, clas)
+        {
+            Name = name;
+            this.lvl = lvl;
+        }
+    }   
+}
---
+++ FabricMethod/FabricMethod/Program.cs
@@ -0,0 +1,173 @@
+﻿namespace FabricMethod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Factory factory = new BakeryPie("Пиржок", "обычный", "100", "вкусный");
+            Product pieDeafault = factory.CreateProduct();
+
+            factory = new BakeryPie("Пиржок", "Необычный", "200", "очень вкусный");
+            Product pie = factory.CreateProduct();
+
+            factory = new BakeryCheburek("Чебурек", "Нормальный", "150", 20);
+            Product cheburek = factory.CreateProduct();
+
+            Product[] products =
+            {
+                pieDeafault, pie, cheburek
+            };
+
+            foreach (Product product in products)
+            {
+                product.DisplayInfo();
+            }
+        }
+    }
+
+    public abstract class Product
+    {
+        protected readonly string _name;
+        protected readonly string _description;
+        protected readonly string _price;
+
+        protected Product(string name, string description, string price)
+        {
+            _name = name;
+            _description = description;
+            _price = price;
+        }
+
+        public abstract void DisplayInfo();
+    }
+
+    public class Pie : Product
+    {
+        private readonly string _tasty;
+        public Pie(string name, string description, string price, string tasty) : base(name, description, price)
+        {
+            _tasty = tasty;
+        }
+
+        public override void DisplayInfo()
+        {
+            Console.WriteLine(
+                $"Название: {_name}\n" +
+                $"Описание: {_description}\n" +
+                $"Цена: {_price}\n" +
+                $"Вкусность: {_tasty}\n"
+                );
+        }
+    }
+
+    public class Bulka : Product
+    {
+
+        private readonly int _weight;
+
+        public Bulka(string name, string description, string price, int weight) : base(name, description, price)
+        {
+            _weight = weight;
+        }
+
+        public override void DisplayInfo()
+        {
+            Console.WriteLine(
+                $"Название: {_name}\n" +
+                $"Описание: {_description}\n" +
+                $"Цена: {_price}\n" +
+                $"Вec: {_weight}\n"
+                );
+        }
+    }
+
+    public class Cheburek : Product
+    {
+        private readonly int _meat;
+        public Cheburek(string name, string description, string price, int meat) : base(name, description, price)
+        {
+            _meat = meat;
+        }
+
+        public override void DisplayInfo()
+        {
+            Console.WriteLine(
+                $"Название: {_name}\n" +
+                $"Описание: {_description}\n" +
+                $"Цена: {_price}\n" +
+                $"мясность: {_meat}\n"
+                );
+        }
+    }
+
+    public abstract class Factory
+    {
+        public abstract Product CreateProduct();
+    }
+
+    public class BakeryPie : Factory
+    {
+
+        protected readonly string _name;
+        protected readonly string _description;
+        protected readonly string _price;
+        private readonly string _tasty;
+
+        public BakeryPie(string name, string description, string price, string tasty)
+        {
+            _name = name;
+            _description = description;
+            _price = price;
+            _tasty = tasty;
+        }
+
+        public override Product CreateProduct()
+        {
+            return new Pie(_name, _description, _price, _tasty);
+        }
+    }
+
+    public class BakeryBulka : Factory
+    {
+
+        protected readonly string _name;
+        protected readonly string _description;
+        protected readonly string _price;
+        private readonly int _weight;
+
+        public BakeryBulka(string name, string description, string price, int weight)
+        {
+            _name = name;
+            _description = description;
+            _price = price;
+            _weight = weight;
+        }
+
+        public override Product CreateProduct()
+        {
+            return new Bulka(_name, _description, _price, _weight);
+        }
+    }
+
+    public class BakeryCheburek : Factory
+    {
+
+        protected readonly string _name;
+        protected readonly string _description;
+        protected readonly string _price;
+        private readonly int _meat;
+
+        public BakeryCheburek(string name, string description, string price, int meat)
+        {
+            _name = name;
+            _description = description;
+            _price = price;
+            _meat = meat;
+        }
+
+        public override Product CreateProduct()
+        {
+            return new Cheburek(_name, _description, _price, _meat);
+        }
+    }
+}
