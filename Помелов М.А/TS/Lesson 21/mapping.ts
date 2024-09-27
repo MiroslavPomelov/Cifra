ad92546898e5ffd8a64418102ccca6d50fd984f4
Miroslav Pomelov <128490138+MiroslavPomelov@users.noreply.github.com>
2024-09-27 21:17:14 +0200

    Update mapping.ts

--- Помелов М.А/TS/Lesson 21/mapping.ts
+++ Помелов М.А/TS/Lesson 21/mapping.ts
@@ -113,6 +113,7 @@
 
 
 
-type Setters<Type> = {
-    [Property in keyof Type as `set${Capitalize<string & Property>}`]: (value: Type[Property]) => void;
-}
+// type Setters<Type> = {
+//     [Property in keyof Type as `set${Capitalize<string & Property>}`]: (value: Type[Property]) => void;
+// }
+
