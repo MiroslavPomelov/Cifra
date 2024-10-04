4ba5c29aecc13b47ab2de1bc11f538f3f01309dc
Miroslav Pomelov <128490138+MiroslavPomelov@users.noreply.github.com>
2024-10-04 20:06:20 +0200

    upd

---
+++ Помелов М.А/TS/Lesson 27 set map iterator/example.js
@@ -0,0 +1,60 @@
+"use strict";
+// // Map
+// const map: Map<string, number> = new Map<string, number>();
+// map.set('first', 25);
+// map.set('second', 45);
+// map.set('third', 35);
+// map.set('fourth', 20);
+// if (map.has('second')) {
+//     console.log('Ok')
+// }
+// else {
+//     console.log('Ne ok')
+// }
+// console.log(map.get('second'));
+// console.log(map.size);
+// // Цикл for of итерирует ключам
+// for (let key of map.keys()) {
+//     console.log(key);
+// }
+// // Цикл for of итерирует значению
+// for (let value of map.keys()) {
+//     console.log(value);
+// }
+// // #1 Цикл for of итерирует коллекцию
+// for (let pair of map) {
+//     console.log(pair);
+// }
+// // #2 Цикл for of итерирует коллекцию
+// for (let [key, value] of map.entries()) {
+//     console.log(`Key ${key} \t Value ${value}`);
+// }
+// map.forEach((value, key) => {
+//     console.log(value);
+// });
+// map.clear();
+// let unusualMap: Map<object, boolean> = new Map<object, boolean>();
+// const obj1 = {
+//     name: 'Valeiy',
+//     age: 32
+// }
+// const obj2 = {
+//     name: 'Inokentiy',
+//     age: 16
+// }
+// unusualMap.set(obj1, obj1.age > 18);
+// unusualMap.set(obj2, obj2.age > 18);
+// console.log(unusualMap.get(obj1));
+// SET
+let arr = [1, 10, 23, 44, 44, 55];
+let set = new Set(arr);
+console.log(set);
+// set.add(20);
+// set.add(1212);
+// set.add(22);
+// set.add(2333);
+// console.log(set);
+// console.log(set.has(6));
+// console.log(set.has(20));
+// console.log(set.size);
+// set.clear();
