69a2a79d9fe62ed7f396614a39f6aee61ebd5920
Miroslav Pomelov <128490138+MiroslavPomelov@users.noreply.github.com>
2025-01-22 21:15:30 +0200

    22.01.2025

---
+++ Помелов М.А/NestJS/axiosPractice/students-service/src/app.service.ts
@@ -0,0 +1,20 @@
+import { HttpService } from '@nestjs/axios';
+import { Injectable } from '@nestjs/common';
+import { firstValueFrom } from 'rxjs';
+
+@Injectable()
+export class AppService {
+  
+  constructor(
+    private readonly httpService: HttpService
+  ) { }
+
+  public async fetchDataFromApi(url: string): Promise<any> {
+    try {
+      const response = await firstValueFrom(this.httpService.get(url));
+      return response.data;
+    } catch (error) {
+      throw new Error(`Ошибка при запросе: ${error.message}`);
+    }
+  }
+}
