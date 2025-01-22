9704a944ef7eaf94434eae7c1a03d6a3fc1d8f92
Miroslav Pomelov <128490138+MiroslavPomelov@users.noreply.github.com>
2025-01-22 20:52:03 +0200

    22/01/2025

---
+++ Помелов М.А/NestJS/axios/src/app.controller.ts
@@ -0,0 +1,16 @@
+import { Controller, Get, HttpException, HttpStatus } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get('data')
+  public async fetchData() {
+    try {
+      return this.appService.fetchDataFromApi('https://jsonplaceholder.typicode.com/users');
+    } catch (error) {
+      throw new HttpException(error.message, HttpStatus.BAD_REQUEST);
+    }
+  }
+}
