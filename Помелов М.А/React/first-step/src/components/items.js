c8edca9408af34c46d584e035407e08ec050c1e3
Miroslav Pomelov <128490138+MiroslavPomelov@users.noreply.github.com>
2025-02-05 21:05:03 +0200

    React

---
+++ Помелов М.А/React/first-step/src/components/items.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react'
+
+const ItemList = () => {
+    const [item, setItem] = useState();
+    const [inputValue, setInputValue] = useState();
+
+
+    const setItems = () => {
+        setItem([...item, input]);
+    }
+
+    return (
+        <div></div>
+    )
+}
+
+export default ItemList;
