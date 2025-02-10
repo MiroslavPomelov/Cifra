3ef23f96b0a8eacfa98979a3ea588dd086cd188e
Miroslav Pomelov <128490138+MiroslavPomelov@users.noreply.github.com>
2025-02-10 21:18:27 +0200

    Update App.js

--- Помелов М.А/React/lesson3/src/App.js
+++ Помелов М.А/React/lesson3/src/App.js
@@ -45,7 +45,7 @@ import DOMPurify from 'dompurify';
 // body {
 // margin: 0;
 // padding: 0;
-// font-family: 'Arial', sans-serif; 
+// font-family: 'Arial', sans-serif;
 // background-color: #f5f5f5;
 // }
 // `;
