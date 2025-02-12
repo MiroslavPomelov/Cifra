d19fdd3d89c4a8030cfaa3e5a6d2a90f61d85491
Miroslav Pomelov <128490138+MiroslavPomelov@users.noreply.github.com>
2025-02-12 20:53:41 +0200

    Update App.js

--- Помелов М.А/React/lesson5/src/App.js
+++ Помелов М.А/React/lesson5/src/App.js
@@ -1,12 +1,65 @@
+import { useState } from 'react';
 import './App.css';
 import SimpleForm from './components/MultiFieldForm/MultiFieldForm';
 import FormWithValidation from './components/ValidatedForm/ValidatedForm';
 
+function Notification({ unreadMessages }) {
+  return (
+    <div>
+      <h1>U have {unreadMessages} new messages</h1>
+      {unreadMessages > 0 && <p>U have mew messages!</p>}
+    </div>
+  )
+}
+
+function Roles({ role }) {
+  return (
+    <div>
+      <h1>U role {role} </h1>
+      {role == 'admin' && <div><p>Welcome admin!</p> <button>Manage users</button> </div>}
+      {role == 'user' && <button>Welcome User!</button>}
+      {role == 'guest' && <button>Welcome Guest! Pls Log In</button>}
+    </div>
+  )
+}
 
 function App() {
+  const [role, setRole] = useState('guest');
+
+  const handleGetRole = () =>
+    setRole(!role);
+
   return (
-    <FormWithValidation/>
+    <div>
+      <Roles role={role} />
+
+      <button>Set new Role</button>
+    </div>
   );
+
+
+
+
+  // const [unreadMessages, setUnreadMessages] = useState(3);
+
+  // return (
+  //   <div>
+  //     <Notification unreadMessages={unreadMessages} />
+
+  //     <button onClick={() => setUnreadMessages(unreadMessages + 1)}>Add new message</button>
+
+  //     <button onClick={() => setUnreadMessages(0)}>
+  //       Read message
+  //     </button>
+  //   </div>
+
+  // <div>
+  //   {isLoggedIn ? <h1>Welcome!</h1> : <h1>Please log in</h1>}
+  //   <button onClick={() => setIsLoggedIn(!isLoggedIn)}>
+  //     {isLoggedIn ? 'Exit' : 'Enter'}
+  //     </button>
+  // </div>
+  // );
 }
 
 export default App;
