3b9e94172f772ea884e2aa9bbee871693de87de9
Miroslav Pomelov <128490138+MiroslavPomelov@users.noreply.github.com>
2025-02-24 21:16:09 +0200

    Update App.js

--- Помелов М.А/React/lesson15/src/App.js
+++ Помелов М.А/React/lesson15/src/App.js
@@ -1,94 +1,82 @@
+import { useMemo, useReducer, useState } from 'react';
+import './App.css';
 
-// import { useMemo, useState } from 'react';
-// import './App.css';
-
-// let users = [];
-
-// const jsonPlaceHolder = async () => {
-//   console.log('Requesting');
-
-//   users = await fetch('https://jsonplaceholder.typicode.com/users');
-//   return await users.json();
-// }
-
-
-
-// function App() {
-//   const [counter, setCounter] = useState(0);
-//   const [text, setText] = useState('');
-
-//   // Дорогостоящая функция
-//   const computeExpensuveValue = (num) => {
-//     console.log('Computing...');
-//     return num * 2;
-//   }
-
-//   const expensiveValue = useMemo(() => {
-//     return computeExpensuveValue(counter);
-//   }, [counter]);
-
-//   const apiValue = useMemo(() => {
-//     return jsonPlaceHolder();
-//   }, []);
-
-//   const reload = () => {
-//     setText(apiValue);
-//   }
+function reducer(state, action) {
+  switch (action.type) {
+    case 'increment':
+      return { counter: state.counter + 1 }
+    case 'decrement':
+      return { counter: state.counter - 1 }
+    default:
+      return state;
+  }
+}
 
 
-//   return (
-
-//     <div>
-//       <ul>
-//         {users.map((user) => {
-//           <li key={user.id}>{user.name}</li>
-//         })}
-//       </ul>
-
-//       <button onClick={reload}>Go</button>
-//     </div>
-//   );
-// }
-
-
-// export default App;
-
-
-
-
-
-
-import React, { useState, useMemo } from 'react';
-
-const UserList = () => {
-  const [users, setUsers] = useState([]);
-
-  const fetchUsers = async () => {
-
-    const request = await fetch("https://jsonplaceholder.typicode.com/users")
-    const data = await request.json();
-    setUsers(data)
-  };
-
-  const memoized = useMemo(() => {
-    return users
-  }, [users])
+function Counter() {
+  const initialState = { counter: 0 };
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   return (
     <div>
-      <h1>User List</h1>
-      <button onClick={fetchUsers}> Вернуть список
-      </button>
-
-      <ul>
-        {memoized.map((user) => (
-          <li key={user.id}>
-            {user.name} ------ ({user.email})
-          </li>
-        ))}
-      </ul>
+      <p>counter: {state.counter}</p>
+      <button onClick={() =>dispatch({type: 'increment'})}>Increase</button>
+      <button onClick={() =>dispatch({type: 'decrement'})}>Decrease</button>
     </div>
   );
-};
+}
 
-export default UserList;
+function App() {
+
+
+
+  return (
+
+    <div>
+      <Counter/>
+    </div>
+  );
+}
+
+
+export default App;
+
+
+
+
+
+
+// import React, { useState, useMemo } from 'react';
+
+// const UserList = () => {
+//   const [users, setUsers] = useState([]);
+
+//   const fetchUsers = async () => {
+
+//     const request = await fetch("https://jsonplaceholder.typicode.com/users")
+//     const data = await request.json();
+//     setUsers(data)
+//   };
+
+//   const memoized = useMemo(() => {
+//     return users
+//   }, [users])
+
+//   return (
+//     <div>
+//       <h1>User List</h1>
+//       <button onClick={fetchUsers}> Вернуть список
+//       </button>
+
+//       <ul>
+//         {memoized.map((user) => (
+//           <li key={user.id}>
+//             {user.name} ------ ({user.email})
+//           </li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// };
+
+// export default UserList;
