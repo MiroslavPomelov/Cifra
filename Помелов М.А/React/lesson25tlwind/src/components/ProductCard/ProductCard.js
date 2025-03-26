381c99d2d6b5c23c6a9bb0ab1169628390d7ea68
Miroslav Pomelov <128490138+MiroslavPomelov@users.noreply.github.com>
2025-03-26 19:29:36 +0200

    26.03.2025

--- Помелов М.А/React/lesson25tlwind/src/components/ProductCard/ProductCard.js
+++ Помелов М.А/React/lesson25tlwind/src/components/ProductCard/ProductCard.js
@@ -1,8 +1,8 @@
 import style from './modules/productCard.module.css';
 
-function ProductCard({src, description, price}) {
+function ProductCard({ src, description, price }) {
 
-
+    const discount = Math.floor(Math.random() * (50 - 1 + 1)) + min;
     return (
         <div className={style.card}>
             <p className='bg-red-500 w-12 rounded-[50px] relative top-3 left-[210px]'>-20%</p>
---
+++ Помелов М.А/React/test-react-app/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+
+  return (
+    <>
+      <Button variant={'default'}>Default button</Button>
+      <Button variant={'destructive'}>Destructive button</Button>
+    </>
+  )
+}
+
+export default App
