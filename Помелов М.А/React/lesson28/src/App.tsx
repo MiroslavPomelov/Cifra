92b2a24387e3d638afd0306a46ec55a3be0147a3
Miroslav Pomelov <128490138+MiroslavPomelov@users.noreply.github.com>
2025-03-28 21:20:49 +0200

    28.03.2025

--- Помелов М.А/React/lesson28/src/App.tsx
+++ Помелов М.А/React/lesson28/src/App.tsx
@@ -4,6 +4,7 @@ import './App.css'
 
 
 import GameGrid from './components/GameGrid'
+import EventType from './types/EventType.type';
 
 
 function App() {
@@ -11,13 +12,20 @@ function App() {
   const [isAactiveCellChordXctive, setActiveCellChordX] = useState(6);
   const [isAactiveCellChordYctive, setActiveCellChordY] = useState(6);
 
+  const handleButtonPressing = (event: EventType) => {
+    if (event.key == 'ArrowUp') {
+      setActiveCellChordY(isAactiveCellChordYctive - 1);
+    }
+  }
 
 
   return (
     <>
-      <GameGrid activeCellChordX={isAactiveCellChordXctive} activeCellChordY={isAactiveCellChordYctive}>
+      <div onKeyDown={handleButtonPressing }>
+        <GameGrid activeCellChordX={isAactiveCellChordXctive} activeCellChordY={isAactiveCellChordYctive}>
 
-      </GameGrid>
+        </GameGrid>
+      </div>
     </>
   )
 }
