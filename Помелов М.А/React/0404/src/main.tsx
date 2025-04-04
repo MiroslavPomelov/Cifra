827d557a2fad9d8c3d666bf1f4085fbb3183eb40
Miroslav Pomelov <128490138+MiroslavPomelov@users.noreply.github.com>
2025-04-04 19:41:00 +0200

    04.04.2025

--- Помелов М.А/React/0404/src/main.tsx
+++ Помелов М.А/React/0404/src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles/index.css'
 import App from './App.tsx'
+import "@radix-ui/themes/styles.css"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
