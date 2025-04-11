8cd8ed70a1ae20e4482b78b771519cb124185e1c
Miroslav Pomelov <128490138+MiroslavPomelov@users.noreply.github.com>
2025-04-11 21:16:33 +0200

    nextjs

--- Помелов М.А/React/prakt-next/src/app/components/ProductCard.tsx
+++ Помелов М.А/React/prakt-next/src/app/components/ProductCard.tsx
@@ -6,9 +6,9 @@ export type ProductPropsType = {
 
 export default function ProductCard({ name, price, onBuy }: ProductPropsType) {
     return (
-        <div className="shadow-xl flex flex-col items-center justify-center p-2 rounded-xl  w-1/2 mx-auto">
-            <h3 className="font-bold">{name}</h3>
-            <h2 className="text-purple-600 font-bold">  ₽  {price}</h2>
+        <div className="shadow-xl flex flex-col items-center justify-center p-2 rounded-xl w-1/3 mx-auto border border-indigo-600 m-8">
+            <h4 className="font-bold m-2">{name}</h4>
+            <p className="text-purple-600 font-bold m-2">  ₽  {price}</p>
             <button className="bg-purple-700 rounded-xl p-3 text-white font-bold" onClick={onBuy}>Buy</button>
         </div>
     )
