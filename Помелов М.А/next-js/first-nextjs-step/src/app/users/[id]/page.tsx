2457c273bbe660227b6656f9a3757ed8b5b3d3a7
Miroslav Pomelov <128490138+MiroslavPomelov@users.noreply.github.com>
2025-04-11 19:55:06 +0200

    nextjs

---
+++ Помелов М.А/next-js/first-nextjs-step/src/app/users/[id]/page.tsx
@@ -0,0 +1,38 @@
+"use client"
+import { useParams } from "next/navigation";
+type User = {
+    id: number,
+    name: string,
+    age: number
+}
+
+export default function DisplayUser() {
+
+    const users: User[] = [
+        {
+            id: 1,
+            name: 'Slava',
+            age: 25
+        },
+        {
+            id: 2,
+            name: 'Bogdan',
+            age: 20
+        },
+        {
+            id: 3,
+            name: 'Denis',
+            age: 26
+        },
+    ];
+
+    const { id } = useParams();
+    const user: User | undefined = users.find(user => user.id == Number(id))
+    return (
+        <div className="bg-sky-600 rounded-xl flex content-center ">
+            <p>User ID: {id}</p>
+            <p>username: {user?.name}</p>
+            <p>username: {user?.age}</p>
+        </div>
+    )
+}
