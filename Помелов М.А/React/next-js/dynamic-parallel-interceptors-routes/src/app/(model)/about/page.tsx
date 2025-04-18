c13605f9b1e537a79416bafa20b9dda6e34ba72c
Miroslav Pomelov <128490138+MiroslavPomelov@users.noreply.github.com>
2025-04-18 21:09:00 +0200

    18.04.2025

---
+++ Помелов М.А/React/next-js/dynamic-parallel-interceptors-routes/src/app/(model)/about/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PageAAA = () => {
+  return (
+    <div>PageAAA</div>
+  )
+}
+
+export default PageAAA
--- Помелов М.А/React/next-js/dynamic-parallel-interceptors-routes/src/app/users/[id]/page.tsx
+++ Помелов М.А/React/next-js/dynamic-parallel-interceptors-routes/src/app/users/[id]/page.tsx
@@ -1,12 +1,15 @@
+"use client"
+
 import { useRouter } from "next/router";
 
 export default function UserPage() {
-    const router = useRouter();
-    const { id } = router.query;
+    // const router = useRouter();
+    // const { id } = router.query;
 
     return (
         <div>
-            <h1>User Profile with id: {id}</h1>
+            {/* <h1>User Profile with id: {id}</h1> */}
+            jjjjjj
         </div>
     );
 }
