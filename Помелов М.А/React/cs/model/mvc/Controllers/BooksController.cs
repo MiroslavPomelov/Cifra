ba89aa60c6bfba03cc58ea5b45c5e52e336a264b
Miroslav Pomelov <128490138+MiroslavPomelov@users.noreply.github.com>
2025-06-06 18:53:12 +0200

    06.06.2025

---
+++ Помелов М.А/React/cs/model/mvc/Controllers/BooksController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using mvc.Models;
+
+namespace mvc.Controllers
+{
+    public class BooksController : Controller
+    {
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Book book)
+        {
+            return View();
+        }
+    }
+}
