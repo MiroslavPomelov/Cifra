66e95296629b39aacc2ed41cca5e8986e4662794
Miroslav Pomelov <128490138+MiroslavPomelov@users.noreply.github.com>
2025-06-11 18:38:03 +0200

    11.06.2025

---
+++ Помелов М.А/React/cs/Filter11_06_2025/Filter11_06_2025/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Filter11_06_2025.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
